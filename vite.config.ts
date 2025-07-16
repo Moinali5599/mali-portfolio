4eaa3ec935a9014e4fce72f7830ecac6e0ed9599
Mahaboob Ali Chisti Shaik <59311134+Moinali5599@users.noreply.github.com>
2025-07-16 16:28:46 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/mali-portfolio",
+  base: process.env.VITE_BASE_PATH || "/mali-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
